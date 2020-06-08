535d1f11f4479fa9f6d9aed80adaba084788d49a
Tin Nguyen <nttin.tn20.cvn@gmail.com>
2020-06-08 22:39:02 +0700

    Add project files.

---
+++ WebCoreApi/DbContexts/TodoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using WebCoreApi.Models;
+
+namespace WebCoreApi.DbContexts
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        {
+           
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
